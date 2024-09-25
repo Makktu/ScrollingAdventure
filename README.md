98c73863c188bad37da3db3e8a31075a4f302195
Makktu <johnmcna@gmail.com>
2024-09-25 21:08:20 +0100

    working on movement and animations

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ Battle enemies and recruit allies.
 Currently working on the most basic aspect: the swipe-and-scroll mechanic that will move the game and player in the world.
 
 Might be a sci-fi world, might be fantasy, will probably have elements of both.
+
+### Day 2
+
+Some progress on the animations of moving from room to room of the game world. Stuck on how to map the game world to code and check for allowed moves without making a complicated code structure that would take more effort and time to maintain accurately that none of this would really be worth doing.
+
+Tweens are the answer for the animation. Now how to check for legal moves?
+
+Mapping to multi-dimensional arrays is possible, but too fiddly.
+
+Will be looking at a graphical option. I.e. the game only allows movement in a direction if the way is not obstructed.
--- scenes/test_scene_1.gd
+++ scenes/test_scene_1.gd
@@ -2,47 +2,78 @@ extends Control
 
 @onready var cam_position_x = $Camera2D.position.x
 @onready var cam_position_y = $Camera2D.position.y
+@onready var animation_player = $AnimationPlayer
+@onready var this_area = $Area
+@onready var move_timer = $MoveTimer
 
-var starting_location = 1
+var current_location = 1
 
-var walls = [
-	[1, [0,0,0,0]],
-	[2, [1,1,1,0]]
-	]
+var input_given = false
+var x_pos = 0
+var y_pos = 0
+
+# ###############################################
+# KEY: Up, Right, Down, Left
+# 0 = no wall - player can pass
+# 1 = wall - player cannot pass
+var rooms = {
+	1: [2,3,4,5],
+	2: [0,1,0,0],
+	3: [0,0,0,1],
+	4: [1,0,0,0],
+	5: [0,1,0,0],
+	6: [0,0,1,1],
+	7: [1,0,0,0]
+}
+# ###############################################
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("ui_right"):
-		starting_location = 2
-		movement(2)
-	if Input.is_action_just_pressed("ui_left"):
-		movement(4)
-	if Input.is_action_just_pressed("ui_up"):
+	
+	if Input.is_action_just_pressed("ui_right") and !input_given:
+		input_given = true
+		move_timer.start()
 		movement(1)
-	if Input.is_action_just_pressed("ui_down"):
+
+	if Input.is_action_just_pressed("ui_left") and !input_given:
+		input_given = true
+		move_timer.start()
 		movement(3)
+
+	if Input.is_action_just_pressed("ui_up") and !input_given:
+		input_given = true
+		move_timer.start()
+		movement(0)
+
+	if Input.is_action_just_pressed("ui_down") and !input_given:
+		input_given = true
+		move_timer.start()
+		movement(2)
+
 		
 func movement(direction = 99):
-	if direction == 99:
-		print("no movement there")
-	if direction == 1: # up
-		cam_position_y += 1280
-	if direction == 2: # right
-		cam_position_x += 720
-		starting_location = 2
-	if direction == 3: # down
-		cam_position_y -= 1280
-	if direction == 4: # left
-		# check for allowed move
-		print(walls[starting_location - 1])
-		if walls[starting_location - 1][1][3] == 0:
-			print('all ok john')
-			cam_position_x -= 720
-		else:
-			print('no exit')
-	
-	move_camera()
+	move_camera(direction)
 		
-func move_camera():
-	$Camera2D.position.x = cam_position_x
-	$Camera2D.position.y = cam_position_y
+func move_camera(direction):
+	if direction == null:
+		print("no direction")
+	var x_dir = 0
+	var y_dir = 0
+	if direction == 0:
+		y_dir = -1280
+	if direction == 1:
+		x_dir = -720
+	if direction == 2:
+		y_dir = 1280
+	if direction == 3:
+		x_dir = 720
+	x_pos += x_dir
+	y_pos += y_dir
+		
+	# move the world
+	var tween = create_tween()
+	tween.tween_property(this_area, "position", Vector2(x_pos, y_pos), 0.45)
+
+
+func _on_move_timer_timeout():
+	input_given = false
